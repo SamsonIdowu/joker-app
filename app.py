1dbd65d5b500695430cd1943ebd3b48634d73a30
Samson <66468924+SamsonIdowu@users.noreply.github.com>
2023-02-19 04:33:53 +0100

    init

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import os
+import pymongo
+
+app = Flask(__name__)
+
+@app.route('/jokes', methods=['GET'])
+def get_jokes():
+    db = pymongo.MongoClient(os.environ['MONGO_URI'])['jokes']
+    joke = db.jokes.aggregate([{ '$sample': { 'size': 1 } }]).next()
+    return jsonify({'joke': joke['text']})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    
